70dfc1ce15699a6ba5d69524448510c88ede877e
Adrian Chudzynski <adrian-chudzynsk@straightvisions.com>
2019-04-23 18:20:28 +0200

    SVM-217 - SV Pagination + removed is_paged() because it never works

--- lib/frontend/tpl/default.php
+++ lib/frontend/tpl/default.php
@@ -1,12 +1,13 @@
-<div class="<?php echo $this->get_prefix(); ?>">
-	<?php
-	echo get_the_posts_pagination(
-		array(
-			'mid_size'				=> 2,
-			'prev_text'				=> __( 'Previous', $this->get_module_name() ),
-			'next_text'				=> __( 'Next', $this->get_module_name() ),
-			'screen_reader_text'	=> ' ',
-		)
-	);
-	?>
-</div>
+<?php
+$args = array(
+	'mid_size'				=> 2,
+	'prev_text'				=> __( 'Previous', $this->get_module_name() ),
+	'next_text'				=> __( 'Next', $this->get_module_name() ),
+	'screen_reader_text'	=> ' ',
+);
+
+if ( ! empty( get_the_posts_pagination( $args  ) ) ) {
+	echo '<div class="' . $this->get_prefix() . '">';
+	echo get_the_posts_pagination( $args );
+	echo '</div>';
+}
