87475ddf312394ea5900d19b08aa7615fc23f83b
Adrian Chudzynski <adrian-chudzynski@straightvisions.com>
2020-03-05 14:39:50 +0100

    Removed run_type() from setting calls.

--- lib/backend/tpl/settings.php
+++ lib/backend/tpl/settings.php
@@ -6,26 +6,26 @@
         <h3 class="divider"><?php _e( 'Pagination', 'sv100' ); ?></h3>
         <div class="sv_setting_flex">
 			<?php
-			echo $module->get_settings_component( 'font_family' )->run_type()->form();
-			echo $module->get_settings_component( 'font_size' )->run_type()->form();
-			echo $module->get_settings_component( 'text_color' )->run_type()->form();
-			echo $module->get_settings_component( 'line_height' )->run_type()->form();
+			echo $module->get_settings_component( 'font_family' )->form();
+			echo $module->get_settings_component( 'font_size' )->form();
+			echo $module->get_settings_component( 'text_color' )->form();
+			echo $module->get_settings_component( 'line_height' )->form();
 			?>
         </div>
         <div class="sv_setting_flex">
 			<?php
-			echo $module->get_settings_component( 'text_deco' )->run_type()->form();
-			echo $module->get_setting( 'text_deco_color' )->run_type()->form();
-			echo $module->get_setting( 'text_deco_thickness' )->run_type()->form();
+			echo $module->get_settings_component( 'text_deco' )->form();
+			echo $module->get_setting( 'text_deco_color' )->form();
+			echo $module->get_setting( 'text_deco_thickness' )->form();
 			?>
         </div>
 
         <h3 class="divider"><?php _e( 'Pagination (Hover/Focus)', 'sv100' ); ?></h3>
         <div class="sv_setting_flex">
 			<?php
-			echo $module->get_settings_component( 'text_color_hover' )->run_type()->form();
-			echo $module->get_settings_component( 'text_deco_hover' )->run_type()->form();
-			echo $module->get_setting( 'text_deco_color_hover' )->run_type()->form();
+			echo $module->get_settings_component( 'text_color_hover' )->form();
+			echo $module->get_settings_component( 'text_deco_hover' )->form();
+			echo $module->get_setting( 'text_deco_color_hover' )->form();
 			?>
         </div>
 		<?php
